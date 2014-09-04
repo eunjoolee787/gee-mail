344f87810785afa1afc72edb8b4cb0dbc41f281b
Eun Joo Lee <eunjoolee787@gmail.com>
2014-09-03 14:54:35 -1000

    Update mail-generator.js

--- js/mail-generator.js
+++ js/mail-generator.js
@@ -29,27 +29,26 @@ function loadGeeMails(){
 var main = function(){
 $('.icon-menu').click(function);
 $('.menu').animate({
-	left: '0px'
-}, 200);
+	console.log(emails.inbox);
+};
 
 $('body').animate({
-	left: '258px'
-}, 200);
-
+	console.log(emails.inbox);
+};
 });
 
 $(document).ready(main);
 $('icon-close').click(function){
 $('icon-close').click(function){
 $('menu').animate({
-	left: '258px'
-}, 200);
+	console.log(emails.inbox);
+};
 
 $('body').animate({
-	left: '200px'
-},200);
-
+	console.log(emails.inbox);
+};
 });
+
 
 
 
